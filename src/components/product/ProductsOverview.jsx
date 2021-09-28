c893f4210eb53d238510abb0b828f5ec6205f98e
leonel47 <waboleonel@gmail.com>
2021-09-28 09:49:27 -0400

    Dynamically change search params

--- src/components/product/ProductsOverview.jsx
+++ src/components/product/ProductsOverview.jsx
@@ -1,6 +1,6 @@
 import { css } from "@emotion/css";
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 import * as ProductService from "./product-service";
 import ProductCard from "./ProductCard";
 
@@ -9,11 +9,13 @@ const productListStyle = css`
     display: flex;
     flex-direction: column;
 
-    .page-title {
-        font-size: 24px;
-        color: #6f6f6f;
-        margin: 8px 0;
-        text-transform: none;
+    .page {
+        &-title {
+            font-size: 24px;
+            color: #6f6f6f;
+            margin: 8px 0;
+            text-transform: none;
+        }
     }
 
     .items {
@@ -30,8 +32,12 @@ function ProductsIndex({ title }) {
     // Access navigation state property
     const { state } = useLocation();
 
+    // Access search parameters
+    const [searchParams, setSearchParams] = useSearchParams();
+
     // get the state after navigation
     useEffect(() => {
+        console.log(searchParams.entries());
         if (state) {
             console.log(
                 `Redirection after an error, unable to find book ${state.id}`
@@ -46,13 +52,65 @@ function ProductsIndex({ title }) {
         })();
     }, []);
 
+    const handleRadioChange = (e) => {
+        const { value, name } = e.target;
+        // get current url parameters
+        const currentParams = Object.fromEntries([...searchParams]);
+        const newParams = { ...currentParams, [name]: value };
+        setSearchParams(newParams);
+    };
+
     if (products === null) {
         return <div>Loading...</div>;
     }
 
     return (
         <div className={productListStyle}>
-            <span className="page-title">{title}</span>
+            <div className="page">
+                <span className="page-title">{title}</span>
+                <div className="page-sort">
+                    <span>Sort</span>
+                    <label>
+                        Name:
+                        <input
+                            type="radio"
+                            name="sort"
+                            value="name"
+                            onChange={handleRadioChange}
+                        />
+                    </label>
+                    <label>
+                        Price:
+                        <input
+                            type="radio"
+                            name="sort"
+                            value="price"
+                            onChange={handleRadioChange}
+                        />
+                    </label>
+                </div>
+                <div className="page-order">
+                    <span>Order</span>
+                    <label>
+                        Ascending:
+                        <input
+                            type="radio"
+                            name="order"
+                            value="asc"
+                            onChange={handleRadioChange}
+                        />
+                    </label>
+                    <label>
+                        Descending:
+                        <input
+                            type="radio"
+                            name="order"
+                            value="desc"
+                            onChange={handleRadioChange}
+                        />
+                    </label>
+                </div>
+            </div>
             <div className="items">
                 {products.books.map((product) => (
                     <ProductCard key={product.id} product={product} />
